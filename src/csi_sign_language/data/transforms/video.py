45a52b218bb2cf1553e68362b5f74e1677e662dc
jingyan <wayenvan@outlook.com>
2024-01-26 16:29:45 +0000

    change transform

--- src/csi_sign_language/data/transforms/video.py
+++ src/csi_sign_language/data/transforms/video.py
@@ -5,18 +5,21 @@ from ...utils.data import *
 from einops import rearrange
 from torchvision import transforms as T
 import torch
-
+import numbers
+import random
+import copy
 
 class Standization:
 
     def __init__(self, mean, std, epsilon=1e-5) -> None:
         """
+        input should be a tensor
         :param mean: size 3, channel means
         :param var: size 3, channel stds
         :param epsilon: -5
         """
-        self.mean = torch.tensor(mean, dtype=torch.float32)
-        self.std = torch.tensor(std, dtype=torch.float32)
+        self.mean = np.array(mean, dtype='float32')
+        self.std = np.array(std, dtype='float32')
         self.epsilon = epsilon
 
     def __call__(self, data) -> Any:
@@ -24,7 +27,7 @@ class Standization:
         #t, c, h, wepsilon
         video = (
             (video - self._rearrange(self.mean)) /
-            torch.sqrt(self._rearrange(self.std)**2 + self.epsilon)
+            np.sqrt(self._rearrange(self.std)**2 + self.epsilon)
         )
         data['video'] = video
         return data
@@ -33,7 +36,7 @@ class Standization:
         return rearrange(x, '(t c h w) -> t c h w', t=1, h=1, w=1)
             
 
-class DownSampleT:
+class TemporalDownSampleT:
     def __init__(self, step) -> None:
         self.s = step
         
@@ -63,7 +66,6 @@ class ToTensor:
     
 
 class CentralCrop:
-
     def __init__(self, size=224) -> None:
         self.size = size
 
@@ -74,4 +76,87 @@ class CentralCrop:
         start_w = math.floor((W - self.size)/2.)
         data['video'] = video[:, :, start_h:start_h+self.size, start_w:start_w+self.size]
         return data
-        
+
+class RandomCrop(object):
+    """
+    Extract random crop of the video.
+    Args:
+        size (sequence or int): Desired output size for the crop in format (h, w).
+        crop_position (str): Selected corner (or center) position from the
+        list ['c', 'tl', 'tr', 'bl', 'br']. If it is non, crop position is
+        selected randomly at each call.
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            if size < 0:
+                raise ValueError('If size is a single number, it must be positive')
+            size = (size, size)
+        else:
+            if len(size) != 2:
+                raise ValueError('If size is a sequence, it must be of len 2.')
+        self.size = size
+
+    def __call__(self, data):
+        clip = data['video']
+
+        crop_h, crop_w = self.size
+        im_c, im_h, im_w = clip[0].shape
+
+        if crop_w > im_w:
+            pad = crop_w - im_w
+            clip = [np.pad(img, ((0, 0), (0, 0), (pad // 2, pad - pad // 2)), 'constant', constant_values=0) for img in
+                    clip]
+            w1 = 0
+        else:
+            w1 = random.randint(0, im_w - crop_w + 1)
+
+        if crop_h > im_h:
+            pad = crop_h - im_h
+            clip = [np.pad(img, ((0, 0), (pad // 2, pad - pad // 2), (0, 0)), 'constant', constant_values=0) for img in
+                    clip]
+            h1 = 0
+        else:
+            h1 = random.randint(0, im_h - crop_h + 1)
+
+        data['video'] = np.stack([img[:, h1:h1 + crop_h, w1:w1 + crop_w] for img in clip])
+        return data
+class RandomHorizontalFlip(object):
+
+    def __init__(self, prob):
+        self.prob = prob
+
+    def __call__(self, data):
+        clip = data['video']
+        # B, H, W, 3
+        flag = random.random() < self.prob
+        if flag:
+            clip = np.flip(clip, axis=-1)
+            clip = np.ascontiguousarray(copy.deepcopy(clip))
+        data['video'] = np.array(clip)
+        return data
+
+class TemporalRescale(object):
+
+    def __init__(self, temp_scaling=0.2, min_len=32, max_len=230):
+        self.min_len = min_len
+        self.max_len = max_len
+        self.L = 1.0 - temp_scaling
+        self.U = 1.0 + temp_scaling
+
+    def __call__(self, data):
+        clip = data['video']
+        vid_len = len(clip)
+        new_len = int(vid_len * (self.L + (self.U - self.L) * np.random.random()))
+        if new_len < self.min_len:
+            new_len = self.min_len
+        if new_len > self.max_len:
+            new_len = self.max_len
+        if (new_len - 4) % 4 != 0:
+            new_len += 4 - (new_len - 4) % 4
+        if new_len <= vid_len:
+            index = sorted(random.sample(range(vid_len), new_len))
+        else:
+            index = sorted(random.choices(range(vid_len), k=new_len))
+        data['video'] = clip[index]
+        return data
